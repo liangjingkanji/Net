3be562cb6b1149033b51a8404b75514a50c8b2eb
drake <liangjinggpx@gmail.com>
2022-03-17 17:27:47 +0800

    修复demo接口字段缺失没有默认值

--- sample/src/main/java/com/drake/net/sample/model/UserModel.kt
+++ sample/src/main/java/com/drake/net/sample/model/UserModel.kt
@@ -3,4 +3,4 @@ package com.drake.net.sample.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UserModel(var name: String, var age: Int, var height: Int)
+data class UserModel(var name: String = "", var age: Int = 0, var height: Int = 0)
