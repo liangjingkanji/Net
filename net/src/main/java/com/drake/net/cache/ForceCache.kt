8ea9d1d18e803a9b0d1fe797310ecafad581f672
drake <liangjinggpx@gmail.com>
2022-07-28 16:45:54 +0800

    fix: #110 缓存模式下Request信息丢失

--- net/src/main/java/com/drake/net/cache/ForceCache.kt
+++ net/src/main/java/com/drake/net/cache/ForceCache.kt
@@ -164,7 +164,7 @@ class ForceCache internal constructor(
         return if (value != null && System.currentTimeMillis() - response.receivedResponseAtMillis > value) {
             null
         } else {
-            response
+            response.newBuilder().request(request).build()
         }
     }
 
