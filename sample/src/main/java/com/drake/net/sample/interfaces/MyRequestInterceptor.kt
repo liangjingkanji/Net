8146bead20cd242538da0f80ac0ba3be32c15eff
drake <liangjinggpx@gmail.com>
2022-09-09 12:06:06 +0800

    sample: 覆盖全局请求头

--- sample/src/main/java/com/drake/net/sample/interfaces/MyRequestInterceptor.kt
+++ sample/src/main/java/com/drake/net/sample/interfaces/MyRequestInterceptor.kt
@@ -2,6 +2,7 @@ package com.drake.net.sample.interfaces
 
 import com.drake.net.interceptor.RequestInterceptor
 import com.drake.net.request.BaseRequest
+import com.drake.net.request.headers
 
 
 /** 请求拦截器, 一般用于添加全局参数 */
@@ -9,7 +10,10 @@ class MyRequestInterceptor : RequestInterceptor {
 
     /** 本方法每次请求发起都会调用, 这里添加的参数可以是动态参数 */
     override fun interceptor(request: BaseRequest) {
-        request.addHeader("client", "Android")
+        // 仅请求动作没有添加时才会添加默认请求头
+        if (request.okHttpRequest.headers()["client"] == null) {
+            request.addHeader("client", "Android")
+        }
         request.setHeader("token", "6cad0ff06f5a214b9cfdf2a4a7c43339")
     }
 }
