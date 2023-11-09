4e6ae7b5b7c32df992e512d9373d40a67c8bb194
drake <liangjinggpx@gmail.com>
2023-11-09 17:42:43 +0800

    sample: UserConfig持久化示例

--- sample/src/main/java/com/drake/net/sample/constants/UserConfig.kt
+++ sample/src/main/java/com/drake/net/sample/constants/UserConfig.kt
@@ -1,12 +1,16 @@
 package com.drake.net.sample.constants
 
+import com.drake.serialize.serialize.annotation.SerializeConfig
+import com.drake.serialize.serialize.serialLazy
+
 
 /**
- * 建议使用 https://github.com/liangjingkanji/Serialize 为字段提供持久化存储
+ * 本单例类使用 https://github.com/liangjingkanji/Serialize 为字段提供持久化存储
  */
+@SerializeConfig(mmapID = "user_config")
 object UserConfig {
 
-    var token = "6cad0ff06f5a214b9cfdf2a4a7c432339"
+    var token by serialLazy(default = "6cad0ff06f5a214b9cfdf2a4a7c432339")
 
-    var isLogin = true
+    var isLogin by serialLazy(default = false)
 }
